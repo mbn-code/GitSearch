8afc534d9623a7dc1e7a41146ed4353ecbba3981
CollinEdward <manorengaard@icloud.com>
2023-08-14 11:30:37 +0200

    Upload dmg (macOS) settings for dmg creation.

---
+++ settings.py
@@ -0,0 +1,18 @@
+app = {
+    'name': 'GitSearch',
+}
+
+
+files = [
+    ('GitSearch.py', 'GitSearch.py'),
+]
+
+
+settings = {
+    'files': files,
+    'symlinks': [('Applications', '/Applications')],
+    'icon_locations': {
+        'GitSearch.py': (200, 200),
+    },
+}
+
